7bede4b212c770116543ad3373ab1034b340d45c
PC671 <1.karimovamadina@gmail.com>
2022-12-21 09:53:30 +0500

    bsb

---
+++ app/src/main/java/uz/itschool/fastfood/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.itschool.fastfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
